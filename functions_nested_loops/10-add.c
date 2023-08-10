0542249342b0b191797b924395a830a96d1846e2
ElizabethCanani <elicanani32@gmail.com>
2023-08-10 11:30:21 -0700

    Write a function that adds two integers and returns the result

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,35 +1,20 @@
 #include "main.h"
 
 /**
- * times_table - print table
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Description: Print 9 times table
- * Return: void
+ * Description: add two integers and return the result.
+ * Return: sum of parameters
  */
 
-void times_table(void)
+int add(int a, int b)
 {
-	int file, column, result;
+	int c;
 
-	for (file = 0; file <= 9; file++)
-	{
-		_putchar('0');
+	c = a + b;
 
-		for (column = 1; column <= 9; column++)
-		{
-			_putchar(',');
-			_putchar(' ');
+	return (c);
 
-			result = file * column;
-
-			if (result <= 9)
-			{
-				_putchar(' ');
-			} else
-				_putchar((result / 10) + '0');
-
-			_putchar((result % 10) + '0');
-			}
-		_putchar('\n');
-	}
 }
