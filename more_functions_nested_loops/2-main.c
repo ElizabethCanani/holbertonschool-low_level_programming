cd9d762acf339cb79b788f3eaa4a6db7ea3176df
ElizabethCanani <elicanani32@gmail.com>
2023-08-15 00:37:37 -0700

    Write a function that multiplies two integers

---
+++ more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%d * %d: %d\n", i, j, mul(i, j));
+    return (0);
+}
