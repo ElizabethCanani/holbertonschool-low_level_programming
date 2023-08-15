7dd4af433ebed2d82bddf0e2f3e2f5e02b163a40
ElizabethCanani <elicanani32@gmail.com>
2023-08-14 22:55:42 -0700

    Write a function that checks for uppercase character

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <ctype.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _isdigit(int c);
+int _isupper(int c);
+void print_triangle(int size);
+
+#endif
