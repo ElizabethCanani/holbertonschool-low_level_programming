bf9ee6218576bc7f4620d1223a1ef1025a2b5868
ElizabethCanani <elicanani32@gmail.com>
2023-08-27 00:09:22 -0700

    Write a function that concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,43 +12,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
-        int s1_len = 0, s2_len = 0, i = 0, j = 0;
-        char *str;
+int s1_len = 0, s2_len = 0, i = 0, j = 0;
+char *str;
 
-        if (s1 == NULL)
-        {
-                s1_len = 1;
-        }
-        else if (s2 == NULL)
-        {
-                s2_len = 1;
-        }
-        else
-        {
-                while (s1[s1_len])
-                {
-                        s1_len++;
-                }
+if (s1 == NULL)
+{
+s1_len = 1;
+}
+else if (s2 == NULL)
+{
+s2_len = 1;
+}
+else
+{
+while (s1[s1_len])
+{
+s1_len++;
+}
 
-                while (s2[s2_len])
-                {
-                        s2_len++;
-                }
-        }
+while (s2[s2_len])
+{
+s2_len++;
+}
+}
 
-        str = (char *) malloc((sizeof(char) * s1_len) + (sizeof(char) * s2_len) + 1);
+str = (char *) malloc((sizeof(char) * s1_len) + (sizeof(char) * s2_len) + 1);
 
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-        for (; i < s1_len; i++)
-        {
-                str[i] = s1[i];
-        }
-        for (; j < s2_len; j++, i++)
-        {
-                str[i] = s2[j];
-        }
-        return (str);
+ if (str == NULL)
+ {
+ return (NULL);
+ }
+for (; i < s1_len; i++)
+{
+str[i] = s1[i];
+}
+for (; j < s2_len; j++, i++)
+{
+str[i] = s2[j];
+}
+return (str);
 }
